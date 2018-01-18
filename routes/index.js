ce8028baa14a9445700cbfe4718cad6cf4251c63
mRcfps <1402491442@qq.com>
2018-01-18 14:22:08 +0800

    Add image cropping.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var crop = require('../util/crop');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("Face Fusion Antiwatermarking Service.");
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.files.image);
+  crop.cropImage(req.files.image.data, function(err, data) {
+    if (err) throw err;
+    res.send(data);
+  });
 });
 
 module.exports = router;
